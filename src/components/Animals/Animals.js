c5ec2e9f3f3382d5d806481596723b53b90a9d32
Lola Simpson <lolalsimp@gmail.com>
2018-07-02 23:53:44 -0500

    showing all 5animals

--- src/components/Animals/Animals.js
+++ src/components/Animals/Animals.js
@@ -4,9 +4,18 @@ import './Animals.css';
 
 class Animals extends React.Component {
   render () {
+    const {animals} = this.props;
+    const animalItemComponents = animals.map((animal) => {
+      return (
+        <li key={animal.id}>{animal.name}</li>
+      );
+    });
     return (
       <div className="Animals">
         <h2>Animals</h2>
+        <ul>
+          {animalItemComponents}
+        </ul>
       </div>
     );
   }
