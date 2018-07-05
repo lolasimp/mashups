15ef6e951f54865e802a37a1e2a3916571a61274
Lola Simpson <lolalsimp@gmail.com>
2018-07-05 09:02:45 -0500

    getting props

--- src/components/AnimalItem/AnimalItem.js
+++ src/components/AnimalItem/AnimalItem.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import './AnimalItem.css';
+import { animalShape } from '../../propz/animalProp';
 
 class AnimalItem extends React.Component {
-  render () {
+  static propTypes = {
+    animal: animalShape,
+  }
+  render() {
+    const { animal } = this.props;
     return (
-      <div className = "AnimalItem">
-        <h2>AnimalItem</h2>
+      <div className="animal-picture">{animal.imgUrl}
+      </div>
+      <div className="animal-name">{animal.name}
+      </div>
+      <div>{animal.description}
       </div>
     );
   }
